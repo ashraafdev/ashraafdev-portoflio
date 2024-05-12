82b68e24b2cf9d93e55198111e8f9922558ecee1
Achraf Hebboul <hebboul.ashraf@gmail.com>
2024-05-12 23:04:20 +0100

    fix typos

--- src/components/Home/header.jsx
+++ src/components/Home/header.jsx
@@ -9,10 +9,10 @@ export default function Header() {
       </div>
       <div className="flex flex-col gap-4 md:gap-2">
         <div className="flex-1 text-center text-[35px] lg:text-[40px] xl:text-[45px]">
-          Welcome to my website when i’ll showcase you my skills
+          Welcome to my website where i’ll showcase you my skills
         </div>
         <div className="flex-1 text-center text-[35px] lg:text-[40px] xl:text-[45px]">
-          I’m a full stack web / mobile developer, searching problems to fix
+          I’m a full stack web / mobile developer, searching for problems to fix
           them{" "}
         </div>
       </div>
