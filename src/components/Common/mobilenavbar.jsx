18db69317656a64dfe2bcf3a135afb60a95d4fcc
Achraf Hebboul <hebboul.ashraf@gmail.com>
2024-05-13 09:28:23 +0100

    fix text size in some pages

--- src/components/Common/mobilenavbar.jsx
+++ src/components/Common/mobilenavbar.jsx
@@ -19,11 +19,11 @@ export default function MobileNavbar() {
         className="w-screen h-screen absolute"
       ></div>
       <div className="absolute w-[75%] h-[700px] top-[400px] left-[50%] -translate-y-[50%] -translate-x-[50%] rounded-md bg-white border border-gray-400 dark:bg-[#393939] z-[1000]">
-        <div className="w-full h-full flex flex-col text-[40px] py-4 gap-6">
-          <div className="flex-1 flex flex-col text-[40px] font-bold items-center justify-center">
+        <div className="w-full h-full flex flex-col text-[35px] py-4 gap-6">
+          <div className="flex-1 flex flex-col font-bold items-center justify-center">
             <div className="h-[70px] w-[70px] rounded-full bg-profile bg-center bg-cover shadow-lg shadow-amber-500/50"></div>
           </div>
-          <div className="flex-1 flex flex-col gap-6 text-[40px] font-bold items-center justify-center">
+          <div className="flex-1 flex flex-col gap-6 font-bold items-center justify-center">
             <button onClick={() => navigate('/')} className="underline underline-offset-4">
               Home
             </button>
